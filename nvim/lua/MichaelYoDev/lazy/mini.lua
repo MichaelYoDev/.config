0121bcf7e7595f5605efdc8b39cbfe433e3141f7
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-06-02 07:13:37 -0700

    fix(nvim): clean plugins

--- nvim/lua/MichaelYoDev/lazy/mini.lua
+++ nvim/lua/MichaelYoDev/lazy/mini.lua
@@ -21,5 +21,18 @@ return { -- Collection of various small independent plugins/modules
             delay = 0,
             draw = { animation = require("mini.indentscope").gen_animation.none() },
         })
+
+        -- Git diff
+        require('mini.diff').setup()
+
+        -- statusline!
+        local statusline = require 'mini.statusline'
+        -- set use_icons to true if you have a Nerd Font
+        statusline.setup { use_icons = vim.g.have_nerd_font }
+
+        ---@diagnostic disable-next-line: duplicate-set-field
+        statusline.section_location = function()
+            return '%2l:%-2v'
+        end
     end,
 }
