b137e697cc54211fdbae625694668687294d9729
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-06-05 07:38:20 -0700

    fix(scripts): refactor for choose folder

---
+++ scripts/choose/webcam.sh
@@ -0,0 +1,1 @@
+ffplay -f avfoundation -framerate 30 -window_title Webcam -video_size 1280x720 -i "FaceTime HD Camera"
--- scripts/script-picker.sh
+++ scripts/script-picker.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 selection=$(
-  ls "$HOME/.config/scripts" |
+  ls "$HOME/.config/scripts/choose" |
     sed 's/\.[^.]*$//' |
     choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92"
 )
 
 if [[ -n "$selection" ]]; then
-  "$HOME/.config/scripts/${selection}.sh"
+  "$HOME/.config/scripts/choose/${selection}.sh"
 fi
