4d0dbe61eb2ba6b88e7ac8c587eb1037a3e51a29
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-06-19 08:28:44 -0700

    feat(scripts): use submodule to manage

--- scripts/choose/apps-picker.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-ls /Applications/ /Applications/Utilities/ /System/Applications/ /System/Applications/Utilities/ | \
-    grep '\.app$' | \
-    sed 's/\.app$//g' | \
-    choose -f "JetBrainsMono Nerd Font" -b "31748f" -c "eb6f92" | \
-    xargs -I {} open -a "{}.app"
--- scripts/term/open-term.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-osascript -e "tell application \"Kitty\" to activate"
-
