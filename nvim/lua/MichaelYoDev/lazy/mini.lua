467ccbbd010c27de10747548ee9792411add95d0
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-07-09 09:05:08 -0700

    fix(nvim): clean plugins

--- nvim/lua/MichaelYoDev/lazy/mini.lua
+++ nvim/lua/MichaelYoDev/lazy/mini.lua
@@ -1,25 +1,17 @@
-return { -- Collection of various small independent plugins/modules
+return {
     'echasnovski/mini.nvim',
     config = function()
-        -- Git diff
         require('mini.diff').setup()
 
-        -- Icons!
         require('mini.icons').setup()
 
-        -- Shows the scope of where you are typing
         require('mini.indentscope').setup({
             symbol = "▏",
-            -- symbol = "│",
             options = { try_as_border = true },
             delay = 0,
             draw = { animation = require("mini.indentscope").gen_animation.none() },
         })
 
-        -- Pair stuff like parenthesis
         require('mini.pairs').setup()
-
-        -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        require('mini.surround').setup()
     end,
 }
