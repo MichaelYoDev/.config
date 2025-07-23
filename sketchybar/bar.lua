b1ba3d8a9bfe49b9deb6fe8cfe39c8dfd137db4e
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-07-23 11:45:07 -0700

    fix: small theme adjustments

--- sketchybar/bar.lua
+++ sketchybar/bar.lua
@@ -7,9 +7,7 @@ sbar.bar({
 	color = colors.bar.bg,
 	padding_right = 2,
 	padding_left = 2,
-	-- blur_radius = 15,
-    blur_radius = 0,
-	-- shadow = true,
-    shadow = false,
+	blur_radius = 15,
+	shadow = true,
 	sticky = true,
 })
