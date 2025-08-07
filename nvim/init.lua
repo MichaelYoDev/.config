e30d71ab7ab410a6856c395d3a127a80bcef7951
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-08-07 00:05:37 -0700

    fix(nvim): simplify config

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@
 local o = vim.opt
 o.guicursor = ""
 o.clipboard = "unnamedplus"
-
 o.number = true
 o.relativenumber = true
 o.tabstop = 4
@@ -10,13 +9,9 @@ o.softtabstop = 4
 o.shiftwidth = 4
 o.expandtab = true
 o.smartindent = false
-
 o.wrap = false
 o.swapfile = false
 o.undofile = true
-o.hlsearch = true
-o.inccommand = "split"
-
 o.termguicolors = true
 o.winborder = "rounded"
 o.scrolloff = 8
@@ -32,25 +27,15 @@ local m = vim.keymap.set
 m("n", "<leader>e", ":Oil<CR>")
 m("n", "<leader>f", ":Pick files<CR>")
 m("n", "<leader>h", ":Pick help<CR>")
+m("n", "<leader>lf", vim.lsp.buf.format)
 m("n", "<leader>m", ":Mason<CR>")
+m("n", "<leader>o", ":update<CR>:source<CR>")
+m("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 m("n", "<leader>te", ":LspTinymistExportPdf<CR>")
 m("n", "<leader>tp", ":TypstPreview<CR>")
-
-m("n", "<leader>o", ":update<CR>:source<CR>")
+m("n", "<leader>u", vim.pack.update)
 m("n", "<leader>w", ":write<CR>")
-m("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 m("n", "<leader>x", ":!chmod +x %<CR>", { silent = true })
-
-m("n", "<leader>lf", vim.lsp.buf.format)
-m("n", "<leader>lk", vim.diagnostic.open_float)
-m("n", "gd", vim.lsp.buf.definition)
-
-m("v", "K", ":m '<-2<CR>gv=gv")
-m("v", "J", ":m '>+1<CR>gv=gv")
-m("n", "J", "mzJ`z")
-
-m("x", "<leader>p", [["_dP]])
-m({ "n", "v" }, "<leader>d", [["_d]])
 
 -- AUTOCMDS
 local aucmd = vim.api.nvim_create_autocmd
@@ -84,45 +69,41 @@ aucmd('LspAttach', {
     end,
 })
 
+vim.lsp.config("lua_ls", {
+    settings = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file("", true) } } }
+})
+
 vim.lsp.config("tinymist", {
     capabilities = capabilities,
     settings = { formatterMode = "typstyle", formatterIndentSize = 4, exportPdf = "onType" },
-})
-
-vim.lsp.config("lua_ls", {
-    settings = { Lua = { workspace = { library = vim.api.nvim_get_runtime_file("", true) } } }
 })
 
 -- PACK
 vim.pack.add({
     { src = "https://github.com/mason-org/mason.nvim" },
     { src = "https://github.com/echasnovski/mini.nvim" },
-    { src = "https://github.com/rose-pine/neovim" },
     { src = "https://github.com/neovim/nvim-lspconfig" },
     { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
     { src = "https://github.com/stevearc/oil.nvim" },
-    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
+    { src = "https://github.com/rose-pine/neovim" },
     { src = "https://github.com/xiyaowong/transparent.nvim" },
     { src = "https://github.com/chomosuke/typst-preview.nvim" },
 })
 
 -- PACK SETUP
-require("rose-pine").setup({ styles = { transparency = true } })
-vim.cmd.colorscheme("rose-pine")
-vim.cmd.hi("statusline guibg=NONE")
-
 require("mason").setup()
 
-require("mini.diff").setup()
+require("oil").setup({ view_options = { show_hidden = true } })
+
 require("mini.icons").setup()
-require("mini.pairs").setup()
-require("mini.surround").setup()
-require("mini.pick").setup()
 require("mini.indentscope").setup({
     symbol = "▏",
     options = { try_as_border = true },
     delay = 0,
     draw = { animation = require("mini.indentscope").gen_animation.none() },
 })
+require("mini.pairs").setup()
+require("mini.pick").setup()
 
-require("oil").setup({ view_options = { show_hidden = true } })
+require("rose-pine").setup({ styles = { transparency = true } })
+vim.cmd.colorscheme("rose-pine")
