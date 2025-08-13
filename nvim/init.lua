d49a6a4537aa92e4ecfee2cee3dea95a96b83cdc
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-08-12 22:58:31 -0700

    feat(nvim): markdown previewing

--- nvim/init.lua
+++ nvim/init.lua
@@ -42,10 +42,11 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 -- PLUGINS =====================================================================
 vim.pack.add({
+    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
     { src = "https://github.com/chomosuke/typst-preview.nvim" },
     { src = "https://github.com/echasnovski/mini.nvim" },
     { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter",          version = "main" },
     { src = "https://github.com/rose-pine/neovim" },
     { src = "https://github.com/stevearc/oil.nvim" },
 })
@@ -77,6 +78,6 @@ m("n", "<leader>pc", funcs.pack_clean)
 m("n", "<leader>pg", funcs.pack_get)
 m("n", "<leader>pu", vim.pack.update)
 m("n", "<leader>s", [[<CMD>%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-m("n", "<leader>v", ':e $MYVIMRC<CR>')
+m("n", "<leader>v", "<CMD>e $MYVIMRC<CR>")
 m("n", "<leader>w", "<CMD>write<CR>")
 m("n", "<leader>x", "<CMD>!chmod +x %<CR>", { silent = true })
