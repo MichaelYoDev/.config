eaad9a9469a7b351972fe6ed80f74b01601b6cb8
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-08-15 20:33:36 -0700

    fix(nvim): remove funcs, replace " with ' cuz why not

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,41 +1,36 @@
 -- SET =========================================================================
 local o = vim.opt
-o.guicursor = ""
+o.guicursor = ''
 o.number = true
 o.relativenumber = true
 o.scrolloff = 8
-o.signcolumn = "yes"
+o.signcolumn = 'yes'
 o.termguicolors = true
-o.winborder = "rounded"
+o.winborder = 'rounded'
 
 o.tabstop = 4
 o.shiftwidth = 4
 o.expandtab = true
 o.smartindent = true
 
-o.clipboard = "unnamedplus"
 o.swapfile = false
 o.undofile = true
-o.undodir = os.getenv("HOME") .. "/.vim/undodir"
+o.undodir = os.getenv('HOME') .. '/.vim/undodir'
 
 o.ignorecase = true
 o.smartcase = true
 
-o.completeopt = { "menu", "fuzzy", "menuone", "noinsert", "popup" }
-
--- LOCAL =======================================================================
-local snippets = require("snippets")
-local funcs = require("funcs")
+o.completeopt = { 'fuzzy', 'menu', 'menuone', 'noinsert', 'popup' }
 
 -- LSP =========================================================================
-vim.lsp.enable({ "bashls", "cssls", "gopls", "html", "jdtls", "lua_ls", "markdown_oxide", "pylsp", "rust_analyzer",
-    "tinymist", "ts_ls" })
+vim.lsp.enable({ 'bashls', 'cssls', 'gopls', 'html', 'jdtls', 'lua_ls', 'markdown_oxide', 'pylsp', 'rust_analyzer',
+    'tinymist', 'ts_ls' })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("lsp-config", { clear = true }),
+vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('lsp-config', { clear = true }),
     callback = function(ev)
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
-        if client:supports_method("textDocument/completion") then
+        if client:supports_method('textDocument/completion') then
             vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
         end
     end,
@@ -43,39 +38,42 @@ vim.api.nvim_create_autocmd("LspAttach", {
 
 -- PLUGINS =====================================================================
 vim.pack.add({
-    { src = "https://github.com/MeanderingProgrammer/render-markdown.nvim" },
-    { src = "https://github.com/chomosuke/typst-preview.nvim" },
-    { src = "https://github.com/echasnovski/mini.nvim" },
-    { src = "https://github.com/mason-org/mason.nvim" },
-    { src = "https://github.com/nvim-treesitter/nvim-treesitter",          version = "main" },
-    { src = "https://github.com/rose-pine/neovim" },
-    { src = "https://github.com/stevearc/oil.nvim" },
+    { src = 'https://github.com/MeanderingProgrammer/render-markdown.nvim' },
+    { src = 'https://github.com/chomosuke/typst-preview.nvim' },
+    { src = 'https://github.com/echasnovski/mini.nvim' },
+    { src = 'https://github.com/mason-org/mason.nvim' },
+    { src = 'https://github.com/nvim-treesitter/nvim-treesitter',          version = 'main' },
+    { src = 'https://github.com/rose-pine/neovim' },
+    { src = 'http ://github.com/shortcuts/no-neck-pain.nvim',              version = 'main' },
+    { src = 'https://github.com/stevearc/oil.nvim' },
 })
 
-require("mason").setup()
-require("mini.icons").setup()
-require("mini.pairs").setup()
-require("mini.pick").setup()
-require("mini.surround").setup()
-require("oil").setup({ view_options = { show_hidden = true }, columns = {} })
-require("rose-pine").setup({ styles = { transparency = true } })
-vim.cmd.colorscheme("rose-pine")
+require('mason').setup()
+require('mini.icons').setup()
+require('mini.pairs').setup()
+require('mini.pick').setup()
+require('mini.surround').setup()
+require('no-neck-pain').setup({ width = 130, buffers = { wo = { fillchars = 'eob: ' } } })
+require('oil').setup({ view_options = { show_hidden = true }, columns = {} })
+require('rose-pine').setup({ styles = { transparency = true } })
+vim.cmd.colorscheme('rose-pine')
+
+-- LOCAL =======================================================================
+local snippets = require('snippets')
 
 -- KEYMAPS =====================================================================
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
 local m = vim.keymap.set
-m("i", "<C-e>", snippets.expand_snippet)
-m("n", "<leader>e", "<CMD>Oil<CR>")
-m("n", "<leader>f", "<CMD>Pick files<CR>")
-m("n", "<leader>h", "<CMD>Pick help<CR>")
-m("n", "<leader>lf", function()
+m('i', '<C-e>', snippets.expand_snippet)
+m('n', '<leader>e', '<CMD>Oil<CR>')
+m('n', '<leader>f', '<CMD>Pick files<CR>')
+m('n', '<leader>h', '<CMD>Pick help<CR>')
+m('n', '<leader>lf', function()
     vim.cmd([[%s/\s\+$//e]])
     vim.lsp.buf.format()
 end)
-m("n", "<leader>m", "<CMD>Mason<CR>")
--- m("n", "<leader>pc", funcs.pack_clean)
-m("n", "<leader>pg", funcs.pack_get)
-m("n", "<leader>pu", vim.pack.update)
-m("n", "<leader>s", [[<CMD>%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-m("n", "<leader>x", "<CMD>!chmod +x %<CR>", { silent = true })
+m('n', '<leader>m', '<CMD>Mason<CR>')
+m('n', '<leader>pu', vim.pack.update)
+m('n', '<leader>zz', '<CMD>NoNeckPain<CR>')
+m({ 'n', 'v' }, '<leader>c', '1z=') -- btw this fixes spelling for word under cursor
