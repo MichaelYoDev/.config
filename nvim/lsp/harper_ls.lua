f922ac749a90fd20e995c65df438932fda5f0e74
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-08-17 20:35:26 -0700

    feat(nvim): trying out harper lsp for writing

---
+++ nvim/lsp/harper_ls.lua
@@ -0,0 +1,10 @@
+return {
+    cmd = { 'harper-ls', '--stdio' },
+    filetypes = {
+        'html',
+        'markdown',
+        'text',
+        'typst',
+    },
+    root_markers = { '.git' },
+}
