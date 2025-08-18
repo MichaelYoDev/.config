f3da9923024c25ee1508b60d3bdadc3fc774cf8d
MichaelYoDev <michaelyoliveira123@gmail.com>
2025-08-17 20:35:53 -0700

    fix(nvim): md and typ ftplugin, fixed typst indent

--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -1,7 +1,9 @@
 vim.keymap.set("n", "<leader>te", "<CMD>LspTinymistExportPdf<CR>", { buffer = 0 })
 vim.keymap.set("n", "<leader>tp", "<CMD>TypstPreview<CR>", { buffer = 0 })
-vim.keymap.set({ "n", "x", "v" }, "j", "gj", { buffer = 0 })
-vim.keymap.set({ "n", "x", "v" }, "k", "gk", { buffer = 0 })
+
+vim.opt_local.shiftwidth = 4
+vim.opt_local.tabstop = 4
+vim.opt_local.expandtab = true
 
 vim.opt_local.linebreak = true
 vim.opt_local.spell = true
